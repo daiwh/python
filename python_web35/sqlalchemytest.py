18ebfeeec95055a2801313b13be1c786ee47cf21
daiwh <daiwenhai2014@163.com>
2016-11-04 00:48:54 +0800

    add query

--- python_web35/sqlalchemytest.py
+++ python_web35/sqlalchemytest.py
@@ -22,10 +22,17 @@ engine = create_engine('mysql+mysqlconnector://root:root@localhost:3306/mytest')
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 # 创建新User对象:
-new_user = User(id='5', name='Bob', password='123456')
+#new_user = User(id='5', name='Bob', password='123456')
 # 添加到session:
-session.add(new_user)
+#session.add(new_user)
 # 提交即保存到数据库:
-session.commit()
+#session.commit()
+# 创建Query查询，filter是where条件，最后调用one()返回唯一行，如果调用all()则返回所有行:
+user = session.query(User).filter(User.id=='5').one()
+# 打印类型和对象的name属性:
+print('type:', type(user))
+print('name:', user.name)
+# 关闭Session:
+session.close()
 # 关闭session:
 session.close()
